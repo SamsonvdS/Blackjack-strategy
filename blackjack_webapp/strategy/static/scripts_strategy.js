acef5a19f4a07ce2471c39b7cef0d92d97bdc2f6
pompoen1 <samson-ik@hotmail.com>
2021-02-03 23:49:49 +0100

    created the counts for cards (doesn't count yet)

---
+++ blackjack_webapp/strategy/static/scripts_strategy.js
@@ -0,0 +1,21 @@
+
+
+var count = 0;
+
+function onClick() {
+    let counterDisplayElem = document.querySelector('.card_button_number');
+    
+    counter++;
+    counterDisplayElem.value = count.toString();
+}
+
+
+
+
+
+
+
+
+
+
+
