a27e6db0030569f21e713e37513e38a79dead985
pompoen1 <samson-ik@hotmail.com>
2021-02-04 23:09:37 +0100

    fixed the counting

--- blackjack_webapp/strategy/static/scripts_strategy.js
+++ blackjack_webapp/strategy/static/scripts_strategy.js
@@ -1,12 +1,12 @@
 
 
 var count = 0;
-
-function onClick() {
-    let counterDisplayElem = document.querySelector('.card_button_number');
-    
-    counter++;
-    counterDisplayElem.value = count.toString();
+// takes one count off of the counter element from a specific card
+function minus_card(element_id) {
+    let counter_element = document.getElementById(element_id + "_number");
+    count = counter_element.value;
+    count++; // verander dit naar count--
+    counter_element.value = count;
 }
 
 
